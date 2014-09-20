9ad314960e5716e8b8976a3afb793bbbd85435e1
ivan.rodriguez.o@gmail.com <sabik@mandroslabs.com>
2014-09-20 14:53:29 -0500

    handlebaring

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-window.Metroruta = Ember.Application.create();
+var App = Ember.Application.create();
+
+Ember.RSVP.configure('onerror', function(error) {
+  if (error instanceof Error) {
+    Ember.Logger.assert(false, error);
+    Ember.Logger.error(error.stack);
+  }
+});
+
+App.Router.map(function() {
+    this.route('mapa');
+    this.route('lineas');
+    this.resource('estacion', { path: '/estacion' }, function() {
+        this.route('buscar');
+        this.route('info');
+        this.route('cercanas');
+    });
+    this.resource('ruta', { path: '/ruta' }, function() {
+        this.route('calcular');
+    });
+});
