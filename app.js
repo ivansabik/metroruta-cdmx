cc91352b00a693abf2fc97f1480ca5f265408917
ivan.rodriguez.o@gmail.com <sabik@mandroslabs.com>
2014-09-22 07:02:46 -0500

    logos new, ctrls mapas y linea con algo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var App = Ember.Application.create();
+App.ApplicationAdapter = DS.FixtureAdapter;
 
 Ember.RSVP.configure('onerror', function(error) {
   if (error instanceof Error) {
@@ -18,4 +19,87 @@ App.Router.map(function() {
     this.resource('ruta', { path: '/ruta' }, function() {
         this.route('calcular');
     });
+});
+
+App.MapaRoute = Ember.Route.extend({
+    model: function() {
+        return Ember.RSVP.hash({lineas: this.store.find('linea')});
+    },
+    setupController: function (controller, model) {
+        controller.set('model', model);       
+    }
+});
+
+App.LineasRoute = Ember.Route.extend({
+    model: function() {
+        return Ember.RSVP.hash({lineas: this.store.find('linea')});
+    }
+});
+
+App.Estacion = DS.Model.extend({
+  nombre:  DS.attr('string'),
+  latitud: DS.attr('number'),
+  longitud: DS.attr('number'),
+  icono: DS.attr('string')
+});
+
+App.Linea = DS.Model.extend({
+  nombre:  DS.attr('string'),
+  colorHex: DS.attr('string'),
+  icono: DS.attr('string'),
+  estaciones: DS.hasMany('estacion', {async:true})
+});
+
+App.MapData = Ember.Object.create({
+	map: null
+});
+
+
+App.MapaView = Ember.ContainerView.extend({
+  id: 'mapa',
+  tagName: 'div',
+  attributeBindings: ['style'],
+  style: "height: 100%; margin: 0px; padding: 0px",
+  map:null,
+  didInsertElement: function() {
+    var latlng = new google.maps.LatLng(19.42705, -99.127571);
+    var mapOptions = {
+      center: latlng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(this.$().get(0),mapOptions);
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+
+    var lineas = this.get('controller.model');
+    console.log(lineas)
+    for(i=0; i<lineas.length; i++) {
+        var color = lineas[i].colorHex;
+        var estaciones = lineas[i].estaciones;
+        var coordenadasLineas = new Array();
+        for(j=0; j<estaciones.length; j++) {
+            coordenadasLineas[j] =  new google.maps.LatLng(estaciones[j].latitud, estaciones[j].longitud);
+            var image = 'imgs/iconosEstaciones/'+estaciones[j].id+'.png';
+            var marcadorEstacion = new google.maps.Marker({
+                position: coordenadasLineas[j],
+                map: map,
+                icon: image
+            });
+            marcadorEstacion.idEstacion = estaciones[j].id;
+            marcadorEstacion.iconoEstacion = estaciones[j].icono;
+            marcadorEstacion.nombreEstacion = estaciones[j].nombre;                        
+        }
+        var caminoLinea = new google.maps.Polyline({
+            path: coordenadasLineas,
+            strokeColor: color,
+            strokeOpacity: 0.6,
+            strokeWeight: 10
+        });
+        caminoLinea.setMap(map);
+    }
+    this.set("map", map);
+  }
 });
---
+++ lineas.js
@@ -0,0 +1,69 @@
+App.Linea.FIXTURES = [
+    {
+        'id':1,
+        'nombre':'L\u00ednea 1',
+        'colorHex':'#F54A91',
+        'icono':'imgs\/iconosLineas\/1.png',
+        'estaciones':[95,127,70,27,125,65,39,13,115,66,102,81,23,121,86,12,18,56,146,99]
+    },{
+        'id':2,
+        'nombre':'L\u00ednea 2',
+        'colorHex':'#0164A8',
+        'icono':'imgs\/iconosLineas\/2.png',
+        'estaciones':[38,98,126,40,106,31,92,118,111,60,15,4,147,169,117,26,140,144,142,89,107,48,55,129]
+    },{
+        'id':3,
+        'nombre':'L\u00ednea 3',
+        'colorHex':'#9E9A36',
+        'icono':'imgs\/iconosLineas\/3.png',
+        'estaciones':[63,41,108,73,134,58,159,71,148,91,61,24,50,51,43,145,36,143,82,35,136]
+    },{
+        'id':4,
+        'nombre':'L\u00ednea 4',
+        'colorHex':'#6FB6AE',
+        'icono':'imgs\/iconosLineas\/4.png',
+        'estaciones':[80,128,16,34,22,87,150,53,69,123]
+    },{
+        'id':5,
+        'nombre':'L\u00ednea 5',
+        'colorHex':'#F7D417',
+        'icono':'imgs\/iconosLineas\/5.png',
+        'estaciones':[105,64,10,162,83,137,154,46,7,96,132,59,166]
+    },{
+        'id':6,
+        'nombre':'L\u00ednea 6',
+        'colorHex':'#DA251C',
+        'icono':'imgs\/iconosLineas\/6.png',
+        'estaciones':[47,133,11,52,93,138,160,78,155,75,163]
+    },{
+        'id':7,
+        'nombre':'L\u00ednea 7',
+        'colorHex':'#E87B14',
+        'icono':'imgs\/iconosLineas\/7.png',
+        'estaciones':[156,6,20,110,173,119,104,9,33,174,122,116,84,14]
+    },{
+        'id':8,
+        'nombre':'L\u00ednea 8',
+        'colorHex':'#00923F',
+        'icono':'imgs\/iconosLineas\/8.png',
+        'estaciones':[54,149,120,170,44,94,152,74,172,37,67,5,2,49,8,68,25,135,32]
+    },{
+        'id':9,
+        'nombre':'L\u00ednea 9',
+        'colorHex':'#4F2926',
+        'icono':'imgs\/iconosLineas\/9.png',
+        'estaciones':[175,100,28,151,77,153,161,85,139,30,109,167]
+    },{
+        'id':10,
+        'nombre':'L\u00ednea A',
+        'colorHex':'#991A91',
+        'icono':'imgs\/iconosLineas\/10.png',
+        'estaciones':[168,3,21,130,57,101,1,124,79,72]
+    },{
+        'id':11,
+        'nombre':'L\u00ednea B',
+        'colorHex':'#a9a9a9',
+        'icono':'imgs\/iconosLineas\/11.png',
+        'estaciones':[19,158,157,76,131,164,171,112,114,165,42,17,141,90,62,113,88,45,97,103,29]
+    }
+];
